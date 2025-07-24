0f0db7daec92c65b6d72f5dfbf4c8d835e302c19
plevada <pedro.levada@mercadolivre.com>
2025-07-24 20:40:34 -0300

    Fix tmp path

--- repository/books_respository.py
+++ repository/books_respository.py
@@ -1,10 +1,11 @@
 from services.write_service import read_from_csv
 from models.Book import Book
+import pathlib
 
 def get_all():
-    data = read_from_csv("tmp/books.csv")
+    data = read_from_csv(pathlib.Path(__file__).resolve() / "tmp" / "books.csv").data
     return list(map(lambda book: Book(**book), data))
 
 def get_by_id(id: int):
-    data = read_from_csv("tmp/books.csv")
+    data = read_from_csv(pathlib.Path(__file__).resolve() / "tmp" / "books.csv")
     return Book(**data[id])
