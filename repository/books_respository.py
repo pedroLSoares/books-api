8eb8bef9fc82c53787772003eebd238161ba2d5f
plevada <pedro.levada@mercadolivre.com>
2025-08-12 06:47:54 -0300

    Rollbacking changes

--- repository/books_respository.py
+++ repository/books_respository.py
@@ -1,14 +1,11 @@
 from services.write_service import read_from_csv, write_to_csv
 from models.Book import Book
 import os
-from services.scrapping_service import Scrapper
+
 
 def get_all():
     if os.path.exists(os.path.join('/tmp', 'books.csv')) == False:
-        """
-         Caso o arquivo não exista, realiza o scraping e salva os dados no arquivo
-        """
-        save_all(Scrapper().extract_data())
+        return []
     
     data = read_from_csv(os.path.join('/tmp', 'books.csv'))
     return list(map(lambda book: Book(**book), data))
